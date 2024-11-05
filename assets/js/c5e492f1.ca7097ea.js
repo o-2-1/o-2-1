edb2224e68a5dd1cb3265925389915aa697b2c65
o-2-1 <o-2-1@users.noreply.github.com>
2024-11-05 16:54:46 +0000

    deploy: e7bb71c0e6deec337dab088f6629a4a49c6dcfe8

--- assets/js/c5e492f1.ca7097ea.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[4802],{780:e=>{e.exports=JSON.parse('{"authors":[{"name":"\u0411\u043e\u0440\u0438\u0441 \u0410\u0441\u0435\u0435\u0432","title":"\u0410\u0434\u043c\u0438\u043d\u0438\u0441\u0442\u0440\u0430\u0442\u043e\u0440","imageURL":"img/logo.png","key":"aseev","page":null,"count":1}]}')}}]);
