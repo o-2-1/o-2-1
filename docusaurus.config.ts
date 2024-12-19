9235c4488145e2a82001109e4b11ccd8aaeea798
setunev.oleg <setunev.oleg@ok20.xyz>
2024-12-19 18:46:48 +0300

    Added the draft of article about communism

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,6 +22,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 const config: Config = {
   title: 'Организация 21',
   tagline: 'Пролетарии всех стран, соединяйтесь!',
@@ -92,7 +95,15 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
-    
+  stylesheets: [
+  {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
     plugins: [
 	[
 	    '@docusaurus/plugin-content-docs',
@@ -103,7 +114,8 @@ const config: Config = {
 		showLastUpdateAuthor: false,
 		showLastUpdateTime: false,
 		sidebarPath: require.resolve('./sidebars.js'),
-		remarkPlugins: [[remarkTypograf, { typograf: myTypograf, builtIn: false }]],
+	    	remarkPlugins: [remarkMath, [remarkTypograf, { typograf: myTypograf, builtIn: false }]],
+	    	rehypePlugins: [rehypeKatex],
 	    },
 	],
 	require.resolve("docusaurus-plugin-image-zoom"),
